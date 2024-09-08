d576b5d9019fc2a21d52adcd592b20a49f13486f
Nineck <nhournoux@uanl.edu.mx>
2024-09-08 16:49:36 -0600

    Reset

--- main.py
+++
@@ -1,7 +0,0 @@
-from website import create_app
-
-app = create_app()
-
-# Al ejecutar este archivo desde este archivo, no desde un importacion, se ejecuta la funcion
-if __name__ == '__main__':
-    app.run(debug=True)
--- website/views.py
+++
@@ -1,9 +0,0 @@
-# Aqui van todas las rutas
-from flask import Blueprint, render_template
-
-views = Blueprint('views', __name__)
-
-@views.route('/')
-def home():
-    return render_template("home.html")
-
